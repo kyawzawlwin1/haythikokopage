cc624d1d3d4530b4b20cfdd749388781ad821ee9
ProGrammerK197 <khirokygn897@gmail.com>
2026-03-19 16:31:30 +0630

    feat:add some paragraphs

--- src/features/static/components/HeroSection.jsx
+++ src/features/static/components/HeroSection.jsx
@@ -5,12 +5,19 @@ const HeroSection = () => {
   return (
     /* flex-1 take the space between header and footer */
     <section
-      className="relative flex-1 w-full bg-cover bg-center bg-no-repeat min-h-screen"
+      className="relative flex w-full bg-cover bg-center bg-no-repeat min-h-screen"
       style={{ backgroundImage: `url(${heroBg})` }}
     >
       {/* <div className="absolute inset-0 bg-black/1"></div> */}
-      <div className="relative z-10 text-white flex">
-        <div className="flex">Hello beauties</div>
+      <div className="relative z-10 text-white flex flex-col items-center justify-center">
+        {/* beauty paragraph */}
+        <div className="flex">
+          <p className="font-bold text-gray-800">Hello beauties</p>
+        </div>
+        {/* another paragraph */}
+        <div>
+          <p></p>
+        </div>
         <h1 className="text-gray-800"></h1>
       </div>
     </section>
