cbd535b11ddd22acc87244d638e4c46f936732cc
ProGrammerK197 <khirokygn897@gmail.com>
2026-03-31 09:38:08 +0630

    fix:some merged

--- src/features/static/components/Footer.jsx
+++ src/features/static/components/Footer.jsx
@@ -8,9 +8,8 @@ const Footer = () => {
     // bg-pink-500/10: soft pink glass effect
     // border-pink-200/20: adding soft pink at borders
 
-    <footer className="bg-secondary-light-yello not-last-of-type:w-full py-12 backdrop-blur-2xl border-t border-pink-300/30">
-      <div className="max-w-3xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center gap-8">
-        Stashed changes
+    <footer className="flex justify-center bg-secondary-light-yello not-last-of-type:w-full py-12 backdrop-blur-2xl border-t border-pink-300/30">
+      <div className=" text-gray-700 max-w-8xl w-full mx-auto px-6 flex flex-col md:flex-row justify-between items-center gap-8">
         {/* Brand Name with Metallic Gradient Text */}
         <div className="font-poppins">
           <span className="text-gray-800 font-bold text-xl uppercase ">
@@ -22,32 +21,34 @@ const Footer = () => {
           </p>
         </div>
         {/* Metallic Social Icons */}
-        <ul className="flex items-center gap-8">
-          <li>
-            <a
-              href="#"
-              className="group relative p-3 bg-white/5 rounded-full border border-pink-200/20 hover:border-pink-300/60 transition-all duration-300 shadow-[inset_0_1px_1px_rgba(255,255,255,0.2)]"
-            >
-              <FaLinkedinIn className="text-pink-200 group-hover:text-white group-hover:drop-shadow-[0_0_8px_rgba(255,255,255,0.8)] text-2xl transition-all" />
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="group relative p-3 bg-white/5 rounded-full border border-pink-200/20 hover:border-pink-300/60 transition-all duration-300 shadow-[inset_0_1px_1px_rgba(255,255,255,0.2)]"
-            >
-              <MdOutlineMailOutline className="text-pink-200 group-hover:text-white group-hover:drop-shadow-[0_0_8px_rgba(255,255,255,0.8)] text-2xl transition-all" />
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="group relative p-3 bg-white/5 rounded-full border border-pink-200/20 hover:border-pink-300/60 transition-all duration-300 shadow-[inset_0_1px_1px_rgba(255,255,255,0.2)]"
-            >
-              <FaSquareInstagram className="text-pink-200 group-hover:text-white group-hover:drop-shadow-[0_0_8px_rgba(255,255,255,0.8)] text-2xl transition-all" />
-            </a>
-          </li>
-        </ul>
+        <div>
+          <ul className="flex items-center gap-8">
+            <li>
+              <a
+                href="#"
+                className="group relative p-3 bg-white/5 rounded-full border border-pink-200/20 hover:border-pink-300/60 transition-all duration-300 shadow-[inset_0_1px_1px_rgba(255,255,255,0.2)]"
+              >
+                <FaLinkedinIn className="text-gray-700 group-hover:text-white group-hover:drop-shadow-[0_0_8px_rgba(255,255,255,0.8)] text-2xl transition-all" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="group relative p-3 bg-white/5 rounded-full border border-pink-200/20 hover:border-pink-300/60 transition-all duration-300 shadow-[inset_0_1px_1px_rgba(255,255,255,0.2)]"
+              >
+                <MdOutlineMailOutline className="text-gray-700 group-hover:text-white group-hover:drop-shadow-[0_0_8px_rgba(255,255,255,0.8)] text-2xl transition-all" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="group relative p-3 bg-white/5 rounded-full border border-pink-200/20 hover:border-pink-300/60 transition-all duration-300 shadow-[inset_0_1px_1px_rgba(255,255,255,0.2)]"
+              >
+                <FaSquareInstagram className="text-gray-700 group-hover:text-white group-hover:drop-shadow-[0_0_8px_rgba(255,255,255,0.8)] text-2xl transition-all" />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
