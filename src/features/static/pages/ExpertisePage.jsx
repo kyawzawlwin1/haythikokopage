012e6427e6c9bf279e8618cda3eaf16428af79c1
ProGrammerK197 <khirokygn897@gmail.com>
2026-05-03 23:39:53 +0630

    fix: something that conflicts

--- src/features/static/pages/ExpertisePage.jsx
+++ src/features/static/pages/ExpertisePage.jsx
@@ -4,6 +4,8 @@ const ExpertisePage = () => {
   return (
     <div className='min-h-screen bg-gray-800'>
       <h1 className='text-white'>expertisepage</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti vel amet consequatur inventore iste, unde at voluptatibus ut eius illum sed dignissimos non, enim earum dolores tempora soluta autem recusandae?
+      </p>
     </div>
   )
 }
