9f60b5d2dc12ed6edcf042fc5e15965c587041d5
ProGrammerK197 <khirokygn897@gmail.com>
2026-05-12 10:55:51 +0630

    fix: fixing routes and names

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { Route, Switch } from "wouter";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
-import ProjectPage from "./pages/ProjectPage";
-import ExpertisePage from "./pages/ExpertisePage";
 import AboutPage from "./pages/AboutPage";
+import ServicesPage from "./pages/ServicesPage";
+import ProjectPage from "./pages/ProjectPage";
 
 const App = () => {
   return (
@@ -16,8 +16,8 @@ const App = () => {
         <div className="min-h-screen">
           <Switch>
             <Route path="/" component={HomePage} />
-            <Route path="/portfolio" component={ProjectPage} />
-            <Route path="/expertise" component={ExpertisePage} />
+            <Route path="/projects" component={ProjectPage} />
+            <Route path="/services" component={ServicesPage} />
             <Route path="/about" component={AboutPage} />
           </Switch>
         </div>
