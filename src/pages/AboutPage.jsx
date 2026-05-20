8bd0915c2e342915acd3bfa4754d64c47aeff63e
ProGrammerK197 <khirokygn897@gmail.com>
2026-05-20 19:00:02 +0630

    feat:added an image

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,23 +1,28 @@
 import React from "react";
-
+import haythikoko from "../assets/haythikoko.jpg";
 const AboutPage = () => {
   return (
-    <div>
+    <div className="flex items-center justify-between h-[800px] ">
       <h1>aboutpage</h1>
       <div></div>
       <div>
-        <p className="text-gray-900">
+        {/* <p className="text-gray-900">
           With a passion for sustainable design, I believe that every structure
           should tell a story. My approach focuses on harmonizing the
           environment with contemporary architectural elements
-        </p>
+        </p> */}
 
         {/* Professional Bio: သူမရဲ့ ပညာအရည်အချင်းနဲ့ အလုပ်အတွေ့အကြုံ အကျဉ်းချုပ်။
         Vision/Philosophy: သူမရဲ့ ဒီဇိုင်းစတိုင်က ဘယ်လိုမျိုးလဲ? (ဥပမာ-
         Minimalist လား၊ ဒါမှမဟုတ် Sustainable ဖြစ်တာကို အားပေးတာလား?)
         Achievements: ရရှိဖူးတဲ့ ဆုတံဆိပ်တွေ ဒါမှမဟုတ် ထင်ရှားတဲ့ project
         ကြီးတွေအကြောင်း ဒီမှာထည့် */}
-      </div> <img src="haythikoko" alt="" className="" />
+        <img
+          src={haythikoko}
+          alt=""
+          className="h-[550px] w-auto object-cover rounded-base shadow-md"
+        />
+      </div>
     </div>
   );
 };
