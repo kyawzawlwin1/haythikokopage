bdc2cfdfdb299a6d342620014d718c28d324a832
ProGrammerK197 <khirokygn897@gmail.com>
2026-06-01 14:51:23 +0630

    feat:adjusting layouts

--- src/features/static/components/HeroSection.jsx
+++ src/features/static/components/HeroSection.jsx
@@ -5,20 +5,21 @@ const HeroSection = () => {
   return (
     /* flex-1 take the space between header and footer */
     <section className="min-h-screen">
-      <div className=" text-white flex flex-row items-center w-full px-12 md:px-24 justify-between bg-cover bg-no-repeat">
-        {/* word box */}
-        <div className="">
+      {/* layer one */}
+      <div className="text-gray-900 grid grid-cols-1 md:grid-cols-2 gap-16 w-full px-12 md:px-24 py-16 max-w-7xl mx-auto">
+        {/* title box*/}
+        <div className="flex flex-col justify-start">
           <h1 className="font-Poppins text-6xl font-bold tracking-tighter text-gray-300 mb-8">
             Crafting Digital Spaces
           </h1>
-         
         </div>
 
-        {/* another paragraph */}
+        {/* paragraph */}
         <div
-          className="w-64 h-64 rounded-xl w-96 h-96 md:w-[500px] md:h-[500px] shadow-lg bg-cover bg-center bg-no-repeat"
+          className="flex flex-col justify-start"
           // style={{ backgroundImage: `url(${heroBg})` }}
-        ><p className="font-bold text-gray-800">
+        >
+          <p className="font-poppins text-gray-700 leading-relaxed text-base md:text-lg">
             I am an architect passionate about creating bold, intense, and
             modern designs that leave a lasting impression. My work blends
             creativity, structure, and innovation to transform spaces into
@@ -29,10 +30,21 @@ const HeroSection = () => {
           <p className="text-gray-900"></p>
         </div>
       </div>
+          {/* layer two */}
       <div
         className="w-full h-[400px] bg-cover bg-center bg-no-repeat relative"
         style={{ backgroundImage: `url(${HeroBg})` }}
-      > </div>
+      >
+        {" "}
+      </div>
+      {/* layer three */}
+      <div className="h-80">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam,
+          doloribus.
+        </p>
+        <button className="bg-gray-900">Contact Me</button>
+      </div>
     </section>
   );
 };
